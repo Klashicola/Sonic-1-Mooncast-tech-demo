230d2db491b2979cc8b0cfe8f312d0695baeb3fb
Klashicola <160373955+Klashicola@users.noreply.github.com>
2025-01-23 23:01:54 -0600

    work started, very rough title screen.

--- Demo/gui/main_menu.gd
+++ Demo/gui/main_menu.gd
@@ -2,8 +2,6 @@ extends Control
 
 @export_file("*.tscn", "*.scn") var main_2D:String = "res://Demo/TestZone.tscn"
 @export_file("*.tscn", "*.scn") var test_2D:String = "res://Demo/test_scene.tscn"
-@export_file("*.tscn", "*.scn") var test_3D:String
-@export_file("*.tscn", "*.scn") var main_3D:String
 
 @onready var anim_play:AnimationPlayer = $"AnimationPlayer"
 @onready var vid_player:VideoStreamPlayer = $"intro_video"
@@ -26,14 +24,10 @@ func _on_2d_pressed() -> void:
 	elif not main_2D.is_empty():
 		get_tree().change_scene_to_file(main_2D)
 
-func _on_3d_pressed() -> void:
-	if Input.is_action_pressed(&"y") and not test_3D.is_empty():
-		get_tree().change_scene_to_file(test_3D)
-	elif not main_3D.is_empty():
-		get_tree().change_scene_to_file(main_3D)
 
 func _on_options_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_intro_video_finished() -> void:
+
 	anim_play.play(&"enter_menu")
